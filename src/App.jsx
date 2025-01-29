23afe9be2054b2b1545a21b574f3371e71c99645
AKASH MAITY <akash.maity.csbs25@heritageit.edu.in>
2025-01-29 20:28:13 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <button className="btn w-64 rounded-full">Button</button>
+    </>
+  )
+}
+
+export default App
