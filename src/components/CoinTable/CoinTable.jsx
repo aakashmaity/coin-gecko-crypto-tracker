13b524ccd547e04fcaa66704156aa768ea8cbf2c
AKASH MAITY <akash.maity.csbs25@heritageit.edu.in>
2025-02-02 00:13:17 +0530

    added react query config

--- src/components/CoinTable/CoinTable.jsx
+++ src/components/CoinTable/CoinTable.jsx
@@ -1,7 +1,39 @@
+import { useEffect, useState } from "react"
+import { fetchCoinData } from "../../services/fetchCoinData"
+import { useQuery } from "@tanstack/react-query";
+
 function CoinTable() {
+
+    const [page, setPage] = useState(1);
+    const { data, status, error } = useQuery({
+        queryKey: ['coins', page],
+        queryFn: () => fetchCoinData(page, 'usd'),
+        retry: 3,
+        retryDelay: 1000,
+        staleTime: 1000
+    })
+
+    useEffect(() => {
+        // console.log(data)
+    },[data])
+
+    if (status === 'pending') {
+        return <span>Loading...</span>
+    }
+
+    if (status === 'error') {
+        return <span>Error: {error.message}</span>
+    }
+
+
     return (
         <div>
+            {data.map((coin) => <div key={coin.id}>{coin.name}</div>)}
             CoinTable
+            <br />
+            <button onClick={() => setPage(page - 1)}>Prev</button>
+            <br />
+            <button onClick={() => setPage(page + 1)}>Next</button>
         </div>
     )
 }
