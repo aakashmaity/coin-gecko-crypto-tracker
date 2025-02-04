ac4e485327317f1729faec29a42524d2ff01de90
AKASH MAITY <akash.maity.csbs25@heritageit.edu.in>
2025-02-05 02:12:20 +0530

    Introducing Zustand

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
 # React + Vite
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+This is a Test project to understand different state management libraries
 
 Currently, two official plugins are available:
 
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+
+
+State management
+
+    1. Introdusing react Context Api
+    2. Using Zustand
--- src/components/CoinTable/CoinTable.jsx
+++ src/components/CoinTable/CoinTable.jsx
@@ -1,14 +1,17 @@
 import { useContext, useState } from "react"
 import { fetchCoinData } from "../../services/fetchCoinData"
 import { useQuery } from "@tanstack/react-query";
-import { CurrencyContext } from "../../context/CurrencyContext";
+// import { CurrencyContext } from "../../context/CurrencyContext";
+import currencyStore from "../../state/store"
 
 function CoinTable() {
 
-    const [page, setPage] = useState(1);
-    const { currency } = useContext(CurrencyContext);
-    
+    // const { currency } = useContext(CurrencyContext);
+    const { currency } = currencyStore();
 
+
+
+    const [page, setPage] = useState(1);
     const { data, status, error , isLoading} = useQuery({
         queryKey: ['coins', page, currency],   
         queryFn: () => fetchCoinData(page, currency),
